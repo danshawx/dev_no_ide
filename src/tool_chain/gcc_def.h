e3e6bd06da6601dc253e33ab5ad2dd4e7a9de8f4
Dan Shaw <xiaodanq@126.com>
2023-09-27 11:11:28 +0800

    Update gcc_def.h

--- src/tool_chain/gcc_def.h
+++ src/tool_chain/gcc_def.h
@@ -1,6 +1,15 @@
 #ifndef GCC_DEF_H
 #define GCC_DEF_H
 
+#ifndef __noasan
+#define __noasan /**/
+#endif
+
+#ifndef __aligned
+#define __aligned(x)	__attribute__((__aligned__(x)))
+#endif
+
+#define __used		__attribute__((__used__))
 
 
 
